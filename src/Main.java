e36fba7128fdfff0edb72bf6ba607751fe00e2ec
Andrey Boyarov <boyarov1995@yandex.ru>
2022-02-15 19:14:07 +0300

    история

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,14 @@ import model.Subtask;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static model.Status.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Subtask> subtaskList = new ArrayList<>();
+        List<Subtask> subtaskList = new LinkedList<>();
         IsMemoryTaskManager manager = new IsMemoryTaskManager();
         IsMemoryHistoryManager isMemoryHistoryManager = new IsMemoryHistoryManager();
 
@@ -20,25 +21,29 @@ public class Main {
         Task task2 = new Task("Задача 1", "Описание", NEW);
         manager.newTask(task1);
         manager.newTask(task2);
-        System.out.println(manager.getTasks());
         isMemoryHistoryManager.add(task1);
         isMemoryHistoryManager.add(task2);
-        System.out.println(isMemoryHistoryManager.getHistory().size());
+
 
         Subtask subtask1 = new Subtask("Подзадача 1", "Описание", NEW, 1);
         Subtask subtask2 = new Subtask("Подзадача 2", "Описание", DONE, 1);
+        subtaskList.add(subtask1);
+        subtaskList.add(subtask2);
         manager.newSubtask(subtask1);
         manager.newSubtask(subtask2);
-        System.out.println(manager.getSubtasks());
         isMemoryHistoryManager.add(subtask1);
         isMemoryHistoryManager.add(subtask2);
-        System.out.println(isMemoryHistoryManager.getHistory().size());
+
+
 
         Epic epic1 = new Epic("Эпик 1", "Описание", NEW, subtaskList);
         manager.newEpic(epic1);
-        System.out.println(manager.getEpics());
         isMemoryHistoryManager.add(epic1);
-        System.out.println(isMemoryHistoryManager.getHistory().size());
+
+
+        for (Integer count : isMemoryHistoryManager.getHistory()) {
+            System.out.println("история " + manager.getTaskById(count) + "\n" + manager.getEpicById(count));
+        }
 
     }
 }
